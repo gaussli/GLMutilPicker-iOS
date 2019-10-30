07c0d5dc20d3a56e48b8f8bbb53694e945816aa5
gaussli <gauss_li@163.com>
2019-10-30 13:07:53 +0800

    Initial Project

---
+++ GLMutilPicker-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLMutilPicker-iOS
+//
+//  Created by lijinhai on 2019/10/30.
+//  Copyright © 2019 gaussli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GLMutilPicker-iOS/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GLMutilPicker-iOS
+//
+//  Created by lijinhai on 2019/10/30.
+//  Copyright © 2019 gaussli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
